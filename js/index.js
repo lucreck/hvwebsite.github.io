024f5607daa0ce6c321ee3025160a5182652a1a9
Loi PT <phamtheloi@gmail.com>
2016-06-25 23:33:32 +0700

    Dark night 2
    Support for all browers

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // $(".character").hover(function(){
+    //     $(".chop div").addClass("animation");
+    // }, function(){
+    //     $(".chop div").removeClass("animation");
+    // });
+  
+    setTimeout(function(){
+      $(".chop div").addClass("animation");
+    }, 3000);
+});
