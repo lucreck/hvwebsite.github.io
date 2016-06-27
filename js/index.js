e4c13c98935b15999b43281be9907156d40edbba
Loi PT <phamtheloi@gmail.com>
2016-06-27 08:13:45 +0700

    DK 2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
-    // $(".character").hover(function(){
-    //     $(".chop div").addClass("animation");
-    // }, function(){
-    //     $(".chop div").removeClass("animation");
-    // });
+    $(".character").hover(function(){
+        $(".character").css(
+        "transform", "scale(" + "1.2,1.2"+ ")"
+        );
+    }, function(){
+      $(".character").css(
+        "transform", "scale(" + "1,1"+ ")"
+        );
+    });
   
     setTimeout(function(){
       $(".chop div").addClass("animation");
-    }, 2500);
+    }, 3000);
+    // var leftMenu = $(window).width()/2 - $(".menu").width()/2;
+    // $(".menu").css("left", leftMenu);
 });
